b48d534eeb2f4cb550d6b935155158fb7516f091
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2023-10-10 08:56:27 +0000

    mas cambios

--- mymovies/mymovies/settings.py
+++ mymovies/mymovies/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mymovies.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "django_bootstrap",
+        "USER": "ubuntu",
+        "PASSWORD": "thisissomeseucrepassword",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
