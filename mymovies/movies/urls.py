cdc24f0e25a80e6c6691c5ac0acd864b432fa747
migueszitle111 <miguel.zitle193@tectijuana.edu.mx>
2023-10-22 02:30:01 +0000

    pantalla genre_movies.html

--- mymovies/movies/urls.py
+++ mymovies/movies/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:movie_id>/", views.movie_detail, name="movie_detail"),
+    path('genre/<int:genre_id>/', views.genre_movies, name='genre_movies'),
 ]
--- mymovies/movies/views.py
+++ mymovies/movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from movies.models import Movie
+from movies.models import Movie,Genre
 # Create your views here.
 
 
@@ -15,3 +15,12 @@ def movie_detail(request, movie_id):
     movie_credits = movie.moviecredit_set.all()  # Obtenemos las MovieCre
     context = {'movie': movie ,'movie_credits': movie_credits}
     return render(request, "movies/movie_detail.html", context=context)
+
+def genre_movies(request, genre_id):
+    genre = Genre.objects.get(pk=genre_id)
+    movies = Movie.objects.filter(genres=genre)
+    context = {
+        'genre': genre,
+        'movies': movies,
+    }
+    return render(request, 'movies/genre_movies.html', context=context)
